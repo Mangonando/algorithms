45f08b2b6b27169fe094ce41092abc49b752f38e
Mangonando <juan.f.ramirezv@gmail.com>
2021-08-27 10:18:18 +0200

    algo of the day

--- meandering-array.js
+++ meandering-array.js
@@ -1,23 +1,16 @@
-//Solution for the popular FizzBuzz question made for Honeypot.io
+//Solution for the popular meandering array question made for Honeypot.io
 
 function meanderingArray(unsorted) {
-    let sorted = [];
-    let clonnedArr = [...unsorted];
-    console.log(clonnedArr)
-    for (let i = 0; i < clonnedArr.length; i++) {
-      if (clonnedArr.length >2) {
-        let max = Math.max(...clonnedArr)
-      console.log(max)
-      let min = Math.min(...clonnedArr)
-      console.log(min)
-      clonnedArr.splice(clonnedArr.indexOf(max), 1)
-      clonnedArr.splice(clonnedArr.indexOf(min), 1)
-      sorted.push(max)
-      sorted.push(min)
-      } else {
+    const clonnedArr = [...unsorted]
+    const sorted = []
+    for (let el of clonnedArr) {
+      const max = Math.max(...clonnedArr)
+      sorted.push(...clonnedArr.splice(clonnedArr.indexOf(max), 1))
+      const min = Math.min(...clonnedArr)
+      sorted.push(...clonnedArr.splice(clonnedArr.indexOf(min), 1))
+      if (clonnedArr.length < 3) {
         sorted.push(clonnedArr[0])
       }
-      
     }
-  return sorted
-}
+    return sorted
+  }
