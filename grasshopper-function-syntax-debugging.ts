cce870d82465685e3ecdffb0662b4fb30588481a
Mangonando <juan.f.ramirezv@gmail.com>
2021-08-29 20:43:19 +0200

    Grasshopper - Function syntax debugging

---
+++ grasshopper-function-syntax-debugging.ts
@@ -0,0 +1,5 @@
+// A student was working on a function and made some syntax mistakes while coding. Help them find their mistakes and fix them.
+
+export function main(verb: string, noun: string): string {
+  return `${verb}${noun}`;
+}
