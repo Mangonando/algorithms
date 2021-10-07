336c301d81eb808d851e756bfd5486527b6a24d5
Mangonando <juan.f.ramirezv@gmail.com>
2021-10-07 22:08:58 +0200

    algo of the day

---
+++ safen-user-input-part-1.js
@@ -0,0 +1,15 @@
+// Your mission is to implement a function that converts the following potentially harmful characters:
+
+// < --> &lt;
+// > --> &gt;
+// " --> &quot;
+// & --> &amp;
+// Good luck :D
+
+function htmlspecialchars(formData) {
+  return formData
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;");
+}
