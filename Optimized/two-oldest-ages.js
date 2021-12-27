ee404838fd2ff2ae7844796cfb60ab1bdb1e79a7
Juan Ramirez <juanramirez@Juans-MacBook-Pro.local>
2021-12-27 12:57:54 +0100

    Optimized algo of the day

---
+++ Optimized/two-oldest-ages.js
@@ -0,0 +1,17 @@
+// The two oldest ages function/method needs to be completed. It should take an array of numbers as its argument and return the two highest numbers within the array. The returned value should be an array in the format [second oldest age, oldest age].
+// The order of the numbers passed in could be any order. The array will always include at least 2 items. If there are two or more oldest age, then return both of them in array format.
+// For example:
+// twoOldestAges( [1, 2, 10, 8] ) // should return [8, 10]
+function twoOldestAges(ages) {
+  var max = 0;
+  var secMax = 0;
+
+  for (i = 0; i < ages.length; i++) {
+    if (ages[i] > max) {
+      max = ages[i];
+    } else if (ages[i] > secMax) {
+      secMax = ages[i];
+    }
+  }
+  return [secMax, max];
+}
