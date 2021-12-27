e00c62e6d21df96b2478463709afc7d398a339ac
Juan Ramirez <juanramirez@Juans-MacBook-Pro.local>
2021-12-27 13:05:10 +0100

    algo of the holiday

--- Optimized/two-oldest-ages.js
+++ Optimized/two-oldest-ages.js
@@ -1,10 +1,9 @@
 // The two oldest ages function/method needs to be completed. It should take an array of numbers as its argument and return the two highest numbers within the array. The returned value should be an array in the format [second oldest age, oldest age].
 // The order of the numbers passed in could be any order. The array will always include at least 2 items. If there are two or more oldest age, then return both of them in array format.
-// For example:
-// twoOldestAges( [1, 2, 10, 8] ) // should return [8, 10]
+// For example: twoOldestAges( [1, 2, 10, 8] ) // should return [8, 10]
 function twoOldestAges(ages) {
-  var max = 0;
-  var secMax = 0;
+  let max = 0;
+  let secMax = 0;
 
   for (i = 0; i < ages.length; i++) {
     if (ages[i] > max) {
--- two-oldest-ages.js
+++ two-oldest-ages.js
@@ -1,12 +1,11 @@
 // The two oldest ages function/method needs to be completed. It should take an array of numbers as its argument and return the two highest numbers within the array. The returned value should be an array in the format [second oldest age, oldest age].
 // The order of the numbers passed in could be any order. The array will always include at least 2 items. If there are two or more oldest age, then return both of them in array format.
-// For example:
-// twoOldestAges( [1, 2, 10, 8] ) // should return [8, 10]
+// For example: twoOldestAges( [1, 2, 10, 8] ) // should return [8, 10]
 function twoOldestAges(ages) {
-    twoOldestAgesResult = []
-    ages.sort(function (a, b) {
-      return b - a
-    })
-    twoOldestAgesResult.push(...ages.splice(0, 2).reverse())
-    return twoOldestAgesResult
-  }
+  twoOldestAgesResult = []
+  ages.sort(function (a, b) {
+    return b - a
+  })
+  twoOldestAgesResult.push(...ages.splice(0, 2).reverse())
+  return twoOldestAgesResult
+}
